f7f321f352780a3b5d0d5ff030b72a4695fc23ae
Ozegya Buhari <ozegyabuhari@gmail.com>
2024-02-01 13:57:15 +0000

    unittest

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,25 @@
+class Utils {
+    static calculateNumber(type, a, b) {
+	const a_rd = Math.round(a);
+	const b_rd = Math.round(b);
+	let c = 0;
+	switch (type) {
+	case 'SUM':
+            c = a_rd + b_rd;
+            break;
+	case 'SUBTRACT':
+            c = a_rd - b_rd;
+            break;
+	case 'DIVIDE':
+	    if (b_rd === 0) {
+		c = "Error";
+	    } else {
+		c = a_rd / b_rd;
+	    }
+	    break;
+	}
+	return c;
+    }
+}
+
+module.exports = Utils;
